ce4a6ba95c61a26a096c72a7e0f5f1e5c5fbd570
mkabumattar <mohammad.khaled@outlook.com>
2022-01-13 19:46:05 +0200

    dev .gitignore

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -45,7 +45,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, '..', './build'),
-    filename: '[name].[hash].js',
+    filename: 'assets/js/[name].[hash].js',
   },
 
   plugins: [
