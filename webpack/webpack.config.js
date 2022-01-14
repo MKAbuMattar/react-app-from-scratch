53ea5867448adb634e26b2c244d14eb5c2a43e41
mkabumattar <mohammad.khaled@outlook.com>
2022-01-14 15:33:56 +0200

    hotfix

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,15 +1,25 @@
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const dotenv = require('dotenv')
+const fs = require('fs')
+const path = require('path')
 const commonConfig = require('./webpack.common.js')
 
-module.exports = (envVars) => {
+module.exports = (envVars, env) => {
   const envConfig = require(`./webpack.${envVars.env}.js`)
 
-  const env = dotenv.config().parsed
+  const currentPath = path.join(__dirname, '..')
 
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next])
+  const basePath = currentPath + '/.env'
+
+  const envPath = basePath + '.' + env.ENVIRONMENT
+
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath
+
+  const fileEnv = dotenv.config({ path: finalPath }).parsed
+
+  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next])
     return prev
   }, {})
 
